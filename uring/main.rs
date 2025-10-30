dc8095f881762f4e1d16165fa754bfd02a188157
Mikhail <svmk17@mail.ru>
2025-10-30 11:22:33 +0300

    add testing with file reading

--- uring/main.rs
+++ uring/main.rs
@@ -1,118 +1,124 @@
-use std::collections::HashMap;
-use std::os::fd::AsRawFd;
+use std::fs::{File, OpenOptions};
+use std::io::Write;
+use std::os::fd::{AsRawFd, FromRawFd, RawFd};
+use std::os::unix::fs::FileExt;
+use std::thread::sleep;
 use std::time::{Duration, Instant};
+use std::{env, fs, mem};
 
-use io_uring::squeue::Entry;
 use io_uring::{IoUring, opcode, types};
-use tokio::sync::mpsc::{UnboundedReceiver, UnboundedSender};
-use tokio::sync::oneshot;
-use uring_bench::{BLOCK_SIZE, SOCKET_AMOUNT, create_writers};
 
-#[tokio::main]
-async fn main() {
-    let writer = std::thread::spawn(|| {
-        create_writers();
-    });
-    let vec = create_readers().await;
-    let (sender, recv) = tokio::sync::mpsc::unbounded_channel();
-    let tasks = tokio::spawn(async move { start_tasks(vec, sender).await });
-    let uring_task = tokio::spawn(async move { start_uring(recv).await });
-    let _ = tokio::join!(uring_task, tasks);
-    writer.join().unwrap();
+fn main() {
+    let vec = set_up_files();
+    read_files_with_ring(vec.clone());
+
+    sleep(Duration::new(5, 0));
+
+    read_with_continuously(vec.clone());
+
+    sleep(Duration::new(5, 0));
+
+    read_with_threads(vec);
+
+    clear();
 }
 
-async fn create_readers() -> Vec<tokio::net::TcpStream> {
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:12456")
-        .await
-        .unwrap();
-    let mut vec = Vec::with_capacity(SOCKET_AMOUNT);
-    for _ in 0..SOCKET_AMOUNT {
-        let (sock, _) = listener.accept().await.unwrap();
-        vec.push(sock);
+const FILE_SIZE: usize = 1024;
+const AMOUNT: usize = 1000;
+fn set_up_files() -> Vec<RawFd> {
+    env::set_current_dir("/home/mihailsvirukov/garbage").unwrap();
+    let buf: &'static mut [u8] = vec![b'a'; FILE_SIZE].leak();
+    let mut files = Vec::with_capacity(AMOUNT);
+    for i in 0..AMOUNT {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(i.to_string())
+            .unwrap();
+        file.write_all(buf).unwrap();
+        files.push(file.as_raw_fd());
+        mem::forget(file)
     }
-    vec
+    files
 }
 
-async fn start_uring(mut recv: UnboundedReceiver<(Entry, oneshot::Sender<usize>)>) {
-    let mut ring = IoUring::new(SOCKET_AMOUNT as u32).unwrap();
-    let mut wait_answers = HashMap::with_capacity(SOCKET_AMOUNT);
-    let mut numb = 0;
-    loop {
-        let mut interval = tokio::time::interval(Duration::from_millis(2));
-        tokio::select! {
-            result = recv.recv() =>
-            {
-                 match result {
-                    Some((entry, reply)) => {
-                        let entry = entry.user_data(numb);
-                        wait_answers.insert(numb, reply);
-                        numb += 1;
-                        unsafe {
-                            ring.submission().push(&entry).unwrap();
-                        }
-                        if ring.submission().len() >= SOCKET_AMOUNT/4 {
-                            ring.submit().unwrap();
-                        }
-                    }
-                    None => {
-                        ring.submit().unwrap();
-                        for result in ring.completion() {
-                            let numb = result.user_data();
-                            let send = wait_answers.remove(&numb).unwrap();
-                            send.send(result.result() as usize).unwrap();
-                        }
-                        return
-                    }
-                }
-            },
-            _ = interval.tick() => {
-                ring.submit().unwrap();
-            }
-        }
-
-        //assert_eq!(res, SOCKET_AMOUNT);
-        for result in ring.completion() {
-            let numb = result.user_data();
-            let send = wait_answers.remove(&numb).unwrap();
-            send.send(result.result() as usize).unwrap();
-        }
-        assert!(ring.completion().is_empty())
+fn clear() {
+    for i in 0..AMOUNT {
+        fs::remove_file(format!("/home/mihailsvirukov/garbage/{i}")).unwrap()
     }
 }
 
-async fn start_tasks(
-    streams: Vec<tokio::net::TcpStream>,
-    sender: UnboundedSender<(Entry, oneshot::Sender<usize>)>,
-) {
-    let blocks = vec![vec![0u8; BLOCK_SIZE]; SOCKET_AMOUNT];
-    let mut vec = Vec::with_capacity(SOCKET_AMOUNT);
+fn read_files_with_ring(files: Vec<RawFd>) {
+    let mut buffers = vec![vec![0u8; FILE_SIZE]; AMOUNT];
+    let mut read = vec![0usize; AMOUNT];
+    let mut ring = IoUring::new(AMOUNT as u32).unwrap();
     let start = Instant::now();
-    for (mut block, stream) in blocks.into_iter().zip(streams) {
-        let value = sender.clone();
-        vec.push(tokio::spawn(async move {
-            let mut amount = 0;
-            loop {
-                let read = opcode::Read::new(
-                    types::Fd(stream.as_raw_fd()),
-                    block[amount..].as_mut_ptr(),
-                    BLOCK_SIZE as u32,
+    loop {
+        let mut changed = 0;
+        for i in 0..AMOUNT {
+            if read[i] < FILE_SIZE {
+                changed += 1;
+                let entry = opcode::Read::new(
+                    types::Fd(files[i]),
+                    buffers[i][read[i]..].as_mut_ptr(),
+                    (FILE_SIZE - read[i]) as u32,
                 )
-                .build();
-                let (send, recv) = oneshot::channel::<usize>();
-                value.send((read, send)).unwrap();
-                let res = recv.await.unwrap();
-                amount += res;
-                if amount == BLOCK_SIZE {
-                    break;
+                .build()
+                .user_data(i as u64);
+                unsafe {
+                    ring.submission().push(&entry).unwrap();
                 }
             }
-        }))
+        }
+        if ring.submission().is_empty() {
+            break;
+        }
+        ring.submit_and_wait(changed).unwrap();
+        while let Some(entry) = ring.completion().next() {
+            let result = entry.result() as usize;
+            let index = entry.user_data();
+            assert!(result > 0);
+            read[index as usize] += result;
+        }
+        assert!(ring.completion().is_empty());
     }
-    for handl in vec {
-        handl.await.unwrap();
-    }
-    let finish = start.elapsed();
     println!(
-        "io-uring finished reading {SOCKET_AMOUNT} sockets {BLOCK_SIZE} bytes each in {finish:?}"
-    )
+        "Uring: Read {AMOUNT} files {FILE_SIZE} each in {:?}",
+        start.elapsed()
+    );
+}
+
+fn read_with_continuously(files: Vec<RawFd>) {
+    let buffers = vec![vec![0u8; FILE_SIZE]; AMOUNT];
+    let start = Instant::now();
+    for (fd, mut buf) in files.into_iter().zip(buffers) {
+        let file = unsafe { File::from_raw_fd(fd) };
+        file.read_exact_at(buf.as_mut_slice(), 0).unwrap();
+        mem::forget(file);
+    }
+    println!(
+        "Continuously: Read {AMOUNT} files {FILE_SIZE} each in {:?}",
+        start.elapsed()
+    );
+}
+
+fn read_with_threads(files: Vec<RawFd>) {
+    let mut threads = Vec::with_capacity(AMOUNT);
+    let buffers = vec![vec![0u8; FILE_SIZE]; AMOUNT];
+    let start = Instant::now();
+    for (fd, mut buf) in files.into_iter().zip(buffers) {
+        threads.push(std::thread::spawn(move || {
+            let file = unsafe { File::from_raw_fd(fd) };
+            file.read_exact_at(buf.as_mut_slice(), 0).unwrap();
+        }));
+    }
+    for thread in threads {
+        thread.join().unwrap();
+    }
+    println!(
+        "Threads: Read {AMOUNT} files {FILE_SIZE} each in {:?}",
+        start.elapsed()
+    );
 }
